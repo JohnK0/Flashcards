3dfb629e33d6700b0dd069de707a4c8e8e7ab500
JohnK0 <johnskim97@gmail.com>
2020-11-20 20:38:48 -0800

    Set constraints and color | Add variables to viewController - Set flashcard to yellow - Set buttons to dark grey

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flashcardLabel: UILabel!
+    @IBOutlet weak var redoButton: UIButton!
+    @IBOutlet weak var memorizedButton: UIButton!
+    @IBOutlet var progressBar: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
