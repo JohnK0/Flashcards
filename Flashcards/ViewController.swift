96aaac6041550160d4c7c68ffc84f74c2e9c1864
JohnK0 <johnskim97@gmail.com>
2020-11-22 19:47:55 -0800

    Add new functionalities to app (hidden text box, submit button, and Next button) - Added hidden text box beneath progress bar - Added submit button at the bottom right of the screen - Renamed Redo button "Next" - Created a Redo button with different functionality than its predecessor that resets the flashcards.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,46 +12,65 @@ class ViewController: UIViewController {
     var tempFlashcards = [String]()
     var total = 0
     var finished = false
+    var empty = true
     
     @IBOutlet weak var flashcardLabel: UILabel!
     @IBOutlet weak var redoButton: UIButton!
     @IBOutlet weak var memorizedButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var submitButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addToFlashcards()
+        checkFlashcardsCount()
         total = flashcards.count
+        if total > 0 {
+            setUpFlashcards()
+            empty = false
+        }
+    }
+    
+    func setUpFlashcards() {
         tempFlashcards = flashcards
         tempFlashcards.shuffle()
-        
         updateProgressBar()
         updateFlashcardLabel(text: tempFlashcards.first!)
     }
     
-    func addToFlashcards() {
-        flashcards.append("Hello")
-        flashcards.append("Bye")
-    }
-    @IBAction func actionButtonPressed(_ sender: UIButton) {
-        print(sender.currentTitle!)
-        if !finished {
-            if sender.currentTitle! == "Memorized" {
-                memorizedButtonPressed()
-                updateProgressBar()
-            }
-            if sender.currentTitle! == "Redo" {
-                redoButtonPressed()
-            }
+    func checkFlashcardsCount() {
+        if flashcards.count == 0 {
+            flashcardLabel.text = "To submit a flashcard,\n open the hidden text box below the progress bar\nand enter in the text.\nThen press the bottom right of the progress bar\n to submit."
         }
-    }
-    func redoButtonPressed() {
-        let temp = tempFlashcards.first!
-        tempFlashcards.removeFirst()
-        tempFlashcards.append(temp)
-        updateFlashcardLabel(text:tempFlashcards.first!)
+        
     }
     
+    @IBAction func actionButtonPressed(_ sender: UIButton) {
+        print(sender.currentTitle!)
+        if !empty {
+            if sender == redoButton {
+                redoButtonPressed()
+            }
+            if !finished {
+                if sender == memorizedButton {
+                    memorizedButtonPressed()
+                    updateProgressBar()
+                }
+                if sender == nextButton {
+                    nextButtonPressed()
+                }
+            }
+        }
+        if sender == submitButton {
+            addNewFlashcard()
+        }
+    }
+    
+    func redoButtonPressed() {
+        finished = false
+        setUpFlashcards()
+    }
     func memorizedButtonPressed() {
         if tempFlashcards.count > 1 {
             tempFlashcards.removeFirst()
@@ -60,6 +79,29 @@ class ViewController: UIViewController {
             finished = true
             tempFlashcards.removeFirst()
             updateFlashcardLabel()
+        }
+    }
+    func nextButtonPressed() {
+        let temp = tempFlashcards.first!
+        tempFlashcards.removeFirst()
+        tempFlashcards.append(temp)
+        updateFlashcardLabel(text:tempFlashcards.first!)
+    }
+    func addNewFlashcard() {
+        guard !(textView.text == nil) else {
+            return
+        }
+        let text = textView.text!
+        if text != "" {
+            if empty == true {
+                updateFlashcardLabel(text: textView.text!)
+                empty = false
+            }
+            flashcards.append(text)
+            tempFlashcards.append(text)
+            total += 1
+            updateProgressBar()
+            textView.text = ""
         }
     }
     
