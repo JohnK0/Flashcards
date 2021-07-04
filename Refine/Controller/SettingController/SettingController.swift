aada00e2c64c1a42a00713e5cf071f7cb1b70d9d
JohnK0 <johnskim97@gmail.com>
2021-07-03 20:32:18 -0500

    Moved lora_bold.ttf to within the Refine directory.

---
+++ Refine/Controller/SettingController/SettingController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingController.swift
+//  Refine
+//
+//  Created by John Kim on 4/17/21.
+//
+
+import UIKit
+
+class SettingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
